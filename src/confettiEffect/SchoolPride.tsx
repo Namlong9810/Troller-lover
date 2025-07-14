013791a8787e171a3ca188a2e374b94bc21026c3
namhm <namhm@iceteasoftware.com>
2025-07-14 16:36:18 +0700

    update: confetti effect

---
+++ src/confettiEffect/SchoolPride.tsx
@@ -0,0 +1,36 @@
+import confetti from "canvas-confetti";
+import { useEffect } from "react";
+
+const SchoolPride: React.FC = () => {
+    useEffect(() => {
+        const end = Date.now() + (15 * 1000);
+
+        // go Buckeyes!
+        const colors = ['#2196f3', '#bb3e03'];
+
+        (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+        }());
+    }, []);
+
+    return null;
+}
+
+export default SchoolPride;
--- src/page/surprisePage.tsx
+++ src/page/surprisePage.tsx
@@ -1,4 +1,6 @@
 import "../styles.css";
+import Fireworks from "../confettiEffect/Firework";
+import SchoolPride from "../confettiEffect/SchoolPride";
 
 export default function surprisePage() {
   return (
@@ -10,6 +12,8 @@ export default function surprisePage() {
         src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjhianNjY3hmY2ZwY2l3c21ya2J4cTV3c3B2a29lNGVnZnA3bGJheCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FAFo1M7EC4gRZ4HETH/giphy.gif"
         alt="Con chó"
       ></img>
+    <Fireworks/>
+    <SchoolPride/>
     </div>
   );
 }
